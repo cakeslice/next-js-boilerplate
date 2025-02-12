fa8598bd5f677dc20f3a8fd2fffdd34734807b0c
José Guerreiro <jose-guerreiro@live.com.pt>
2025-02-12 16:23:00 +0100

    remove next-image-export-optimizer

--- next.config.js
+++ next.config.js
@@ -9,27 +9,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withPlugins([withBundleAnalyzer], {
 	output: process.env.STANDALONE ? 'standalone' : process.env.EXPORT ? 'export' : undefined,
 	reactStrictMode: true,
-	transpilePackages: [
-		'@uidotdev',
-		'@heroicons',
-		...(process.env.EXPORT === 'true' ? ['next-image-export-optimizer'] : []),
-	],
-	...(process.env.EXPORT === 'true' && {
-		images: {
-			loader: 'custom',
-			imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-			deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-		},
-		env: {
-			nextImageExportOptimizer_imageFolderPath: 'public/images',
-			nextImageExportOptimizer_exportFolderPath: 'out',
-			nextImageExportOptimizer_quality: '100',
-			nextImageExportOptimizer_storePicturesInWEBP: 'true',
-			nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
-			nextImageExportOptimizer_generateAndUseBlurImages: 'true',
-			nextImageExportOptimizer_remoteImageCacheTTL: '0',
-		},
-	}),
+	transpilePackages: ['@uidotdev', '@heroicons'],
 	experimental: {
 		swcPlugins: [['@lingui/swc-plugin', {}]],
 	},
